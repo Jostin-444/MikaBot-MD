f61501ca050b4f307f29032fa91732ebd2aba380
Giovanni-OFC <jostingaleano10@gmail.com>
2024-09-23 15:46:16 -0600

    Actualizar main-staff.js

--- plugins/main-staff.js
+++ plugins/main-staff.js
@@ -1,28 +1,28 @@
 let handler = async (m, { conn, command, usedPrefix }) => {
 let staff = `🚩 *EQUIPO DE AYUDANTES*
-🍟 *Bot:* ${global.botname}
+🐦‍⬛ *Bot:* ${global.botname}
 ✨️ *Versión:* ${global.vs}
 
 👑 *Propietario:*
 
 • Diamond
-🍟 *Rol:* Propietario
-🚩 *Número:* wa.me/5351524614
-✨️ *GitHub:* https://github.com/David-Chian
+🐦‍⬛ *Rol:* Propietario
+🚩 *Número:* wa.me/50242073893
+✨️ *GitHub:* https://github.com/Jostin-444
 
-🌸  *Colaboradores:*
+🐦‍⬛  *Colaboradores:* 🐦‍⬛
 
-• Miguelon
-🍟 *Rol:* Developer
-🚩 *Número:* Wa.me/528711426787
+• Giovanni-OFC
+🌟 *Rol:* Developer
+🚩 *Número:* Wa.me/51902855352
 
-• Steven
-🍟 *Rol:* Contribuidor
-🚩 *Número:* Wa.me/593984964830
+• Keni
+🌟 *Rol:* Contribuidor
+🚩 *Número:* Wa.me/5493816457465
 
-• Dino
-🍟 *Rol:* Editor
-🚩 *Número:* Wa.me/527774603921
+• Jostin-444
+🌟 *Rol:* Editor
+🚩 *Número:* Wa.me/50242073893 
 `
 await conn.sendFile(m.chat, icons, 'yaemori.jpg', staff.trim(), fkontak, true, {
 contextInfo: {
