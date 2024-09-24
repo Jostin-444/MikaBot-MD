59732a3d68d0359e46c67ca56e9b2fd2c62b162c
Giovanni-OFC <jostingaleano10@gmail.com>
2024-09-24 17:04:52 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -119,7 +119,7 @@ global.año = d.toLocaleDateString('es', {year: 'numeric'});
 global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
 //* ****************************
 global.wm2 = `${dia} ${fecha}\nMikaBot-MD`;
-global.nomorown = '5351524614';
+global.nomorown = '50242073893';
 global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf'];
 global.cmenut = '❖––––––『';
 global.cmenub = '┊✦ ';
