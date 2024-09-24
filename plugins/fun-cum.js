dd7d529def67b9fdad479036f9be3e50fdb70f03
Giovanni-OFC <jostingaleano10@gmail.com>
2024-09-24 14:41:20 -0600

    Actualizar fun-cum.js

--- plugins/fun-cum.js
+++ plugins/fun-cum.js
@@ -28,7 +28,7 @@ let pp5 = 'https://telegra.ph/file/0103144b636efcbdc069b.mp4'
 let pp6 = 'https://telegra.ph/file/4d97457142dff96a3f382.mp4'
 let pp7 = 'https://telegra.ph/file/b1b4c9f48eaae4a79ae0e.mp4'
 let pp8 = 'https://telegra.ph/file/5094ac53709aa11683a54.mp4'
-let pp9 = 'https://telegra.ph/file/90ad889125a3ba40bceb8.jpg'
+let pp9 = 'https://qu.ax/RyOX.jpg'
 let pp10 = 'https://telegra.ph/file/dc279553e1ccfec6783f3.mp4'
 let pp11 = 'https://telegra.ph/file/acdb5c2703ee8390aaf33.mp4'
 
