5a2c71a9c79a90e6cbb8680b1d9f7183e50a154a
Giovanni-OFC <jostingaleano10@gmail.com>
2024-09-24 13:51:43 -0600

    Actualizar gc-invitar.js

--- plugins/gc-invitar.js
+++ plugins/gc-invitar.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-if (!text) throw `🍟 Ingrese el número al que quiere enviar una invitación al grupo\n\n🚩 Ejemplo :\n*${usedPrefix + command}* 5351524614`
+if (!text) throw `🍟 Ingrese el número al que quiere enviar una invitación al grupo\n\n🚩 Ejemplo :\n*${usedPrefix + command}* 50242073893`
 if (text.includes('+')) throw  `🚩 Ingrese el número todo junto sin el *+*`
 if (isNaN(text)) throw '🍟 Ingrese sólo números más su código de país sin espacios'
 let group = m.chat
