d09959adebdf41ceb1b18d9e9046bb1650301ad4
Giovanni-OFC <jostingaleano10@gmail.com>
2024-09-23 21:36:33 -0600

    Actualizar mods-unbanchat.js

--- plugins/mods-unbanchat.js
+++ plugins/mods-unbanchat.js
@@ -3,7 +3,7 @@ if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '🍭 *¡ESTE C
 let chat = global.db.data.chats[m.chat]
 if (!chat.isBanned) return conn.reply(m.chat, '🍟 *¡MEGUMIN-BOT NO ESTÁ BANEADA EN ESTE CHAT!*', m, fake)
 chat.isBanned = false
-await conn.reply(m.chat, '🚩 *¡MEGUMIN-BOT YA FUÉ DESBANEADA EN ESTE CHAT!*', m, fake)
+await conn.reply(m.chat, '🚩 *¡MikaBot MD YA FUÉ DESBANEADA EN ESTE CHAT!*', m, fake)
 }
 handler.help = ['unbanchat'];
 handler.tags = ['mods'];
