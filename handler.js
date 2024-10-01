b775a77846b5d107f269b1c7a274a2b2e32846ec
Jostin-444 <gemersport7@gmail.com>
2024-10-01 15:12:13 -0600

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -741,7 +741,7 @@ console.error(e)
 
 global.dfail = (type, m, conn) => {
 const msg = {
-rowner: '「👑」 *Esta función solo puede ser usada por mi creador*\n\n> David-Chian.', 
+rowner: '「👑」 *Esta función solo puede ser usada por mi creador*\n\n> Jostin-444.', 
 owner: '「👑」 *Esta función solo puede ser usada por mi desarrollador.', 
 mods: '「🤴🏻」 *Esta función solo puede ser usada por mis desarrolladores.*', 
 premium: '「🍧」 *Esta función solo es para usuarios Premium.', 
