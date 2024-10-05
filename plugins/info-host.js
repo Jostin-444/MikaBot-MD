410d6564fa084ed37a441292b6fbaec21e3ab94c
Jostin-444 <gemersport7@gmail.com>
2024-10-05 01:05:15 -0600

    Actualizar info-host.js

--- plugins/info-host.js
+++ plugins/info-host.js
@@ -1,24 +1,43 @@
+//código editado x Mxz <3
+//▝▞▟▘▙▚▛▜▝▞▟▖▗▘▙▚▛▜▝
 let handler = async (m, { conn, command, usedPrefix }) => {
-let txt = `🚩 *O L Y M P U S - H O S T* 
+let txt = `👋 *¡Bienvenido a TK-Host  🚀💻*
+La plataforma más rápida y confiable para alojar tus servidores y potenciar tus proyectos. 🌐
 
-*¿Quieres un Host de calidad y con bajos precios?*
-Pues te presento a *OlympusHost*, un hosting de calidad con servidores dedicados y precios por debajo de 1USD, estos servidores están destinados a ofrecerte un Uptime 24/7 para que puedas alojar tus proyectos y qué estos funcionen de manera eficaz.
+🔑 *Comienza ahora mismo:*
+- *Regístrate*: [https://dash.tk-joanhost.com]  
+- *Verifica tu cuenta*: revisa spam de Gmail oh que te verifique JoanTK
+- *Crea tu servidor*: [https://dash.tk-joanhost.com/servers]  
+- *Accede a tu servidor*: [https://panel.tk-joanhost.com]
 
-🟢 \`\`\`Información del Host\`\`\`
+💳 *¿Quieres recargar coins?*
 
-🔮 *Dashboard:* 
-• https://dash.olympus-host.xyz
+💡 *Compra tus coins* con *PayPal* de forma rápida y segura:  
+👉 [https://dash.tk-joanhost.com/store]
 
-🧃 *Panel:*
-• https://panel.olympus-host.xyz
+*¿No tienes PayPal?*  
+¡No te preocupes! 🌍 También aceptamos *pagos internacionales**. Solo envía el monto solicitado en la tienda y el comprobante de pago. Activaremos tus *coins* rápidamente. 
 
-💥 *Canal:*
-• https://whatsapp.com/channel/0029ValCkNT2ER6gHWFRQ71J
+📱 *Para usuarios en Perú*:  
+- *Yape/Agora*: 927803866  
+- *Plin*: 976673519  
 
-⚜️ *Contacto (Mateo)*
-https://wa.me/593979133620
+🌍 *Para clientes internacionales*:  
+- *Mercado Pago*: *02JOAN*  
+- *Astropay*: *02JOAN*
 
-> *Únete a está comunidad y disfruta de un servicio de calidad :D*` 
+
+✨ *Gracias por elegir TK-Host.* Estamos comprometidos a brindarte la mejor experiencia en alojamiento de servidores. 🚀 ¡Tu éxito es nuestra prioridad!
+
+---
+
+    💥ᮬ᭄ᮬ᭄ Canal Oficial de TK-HOST:
+[https://whatsapp.com/channel/0029VaGGynJLY6d43krQYR2g]
+
+      📲 Canal de Respaldo:
+[https://whatsapp.com/channel/0029VaoZXbk6RGJNYQVP8r27]
+
+---` 
 await conn.sendMessage(m.chat, { text: txt,
 contextInfo:{
 forwardingScore: 9999999,
@@ -26,14 +45,14 @@ isForwarded: false,
 "externalAdReply": {
 "showAdAttribution": true,
 "containsAutoReply": true,
-title: `🔵 O L Y M P U S - H O S T 🔵`,
-body: `⚜️ Super Hosting 24/7 ⚜️`,
+title: `🔥 亇Ӄ-ん૭ટｲ 🔥`,
+body: `✅ Hosting de Calidad`,
 "previewType": "PHOTO",
-thumbnailUrl: 'https://qu.ax/waCu.jpg', 
-sourceUrl: 'https://dash.olympus-host.xyz'}}},
-{ quoted: fkontak})
+thumbnailUrl: 'https://qu.ax/hXTXJ.jpg', 
+sourceUrl: 'https://dash.tk-joanhost.com'}}},
+{ quoted: fakegif3})
 }
 handler.tags =['main'] 
 handler.help = ['host', 'hosting'] 
-handler.command = ['host', 'olympus', 'olympushost', 'hosting']
+handler.command = ['host', 'tkpanel', 'joanhost', 'hosting']
 export default handler
