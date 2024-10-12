81a4a63ca6a95ebf31e2524de2b8b676d955f869
Giovanni-OFC <jostingaleano10@gmail.com>
2024-10-12 14:25:56 -0600

    Actualizar Guerra.js

--- plugins/Guerra.js
+++ plugins/Guerra.js
@@ -6,7 +6,7 @@ var handler = async (m, { conn, participants, groupMetadata, args, text }) => {
     const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net';
 
     // Verificar si se ingresaron suficientes argumentos (hora) 
-    if (args.length < 2) return m.reply('➤ `𝗔𝗩𝗜𝗦𝗢` ⚜️\n\n*INGRESA UNA HORA Y UNA MODALIDAD*\n_Ejemplo: .guerra 10:00🇲🇽 PM_');
+    if (args.length < 2) return m.reply('➤ `𝗔𝗩𝗜𝗦𝗢` ⚜️\n\n*INGRESA UNA HORA*\n_Ejemplo: .guerra 10:00🇲🇽 PM_');
 
     const hora = args[0]; // Primer texto (hora)
   
