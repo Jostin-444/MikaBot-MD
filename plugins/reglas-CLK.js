3ca32d6572012b0644d295752b61b604221c67d0
Giovanni-OFC <jostingaleano10@gmail.com>
2024-10-12 12:16:32 -0600

    Actualizar reglas-CLK.js

--- plugins/reglas-CLK.js
+++ plugins/reglas-CLK.js
@@ -6,12 +6,15 @@ var handler = async (m, { conn, participants, groupMetadata, args, text }) => {
     const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net';
 
     // Verificar si se ingresaron suficientes argumentos (hora y modalidad)
-    if (args.length < 2) return m.reply('➤ `𝗔𝗩𝗜𝗦𝗢` ⚜️\n\n*INGRESA UNA HORA Y UNA MODALIDAD*\n_Ejemplo: .reglasclk_');
+    if (args.length < 2) return m.reply('➤ `𝗔𝗩𝗜𝗦𝗢` ⚜️\n\n*INGRESA UNA HORA Y UNA MODALIDAD*\n_Ejemplo: .4vs4infimasc 10:00 Infinito_');
 
     const hora = args[0]; // Primer texto (hora)
     const modalidad = args.slice(1).join(' '); // Segundo texto (modalidad), puede contener más de una palabra
 
     m.react('🎮');
+
+    let yo = `│🕓 𝗛𝗢𝗥𝗔: *${hora}*`;
+    let modo = `│🎮 𝗠𝗢𝗗𝗔𝗟𝗜𝗗𝗔𝗗: *${modalidad}*`;
 
     let texto = `
 ┍𝑹𝒆𝒈𝒍𝒂𝒔 𝑪𝑳𝑲🗒️
@@ -56,9 +59,9 @@ var handler = async (m, { conn, participants, groupMetadata, args, text }) => {
     conn.sendFile(m.chat, pp, 'error.jpg', texto, m, true, { mentions: [...groupAdmins.map(v => v.id), owner] });
 
 }
-handler.help = ['reglasclk']
-handler.tags = ['ffreglas']
-handler.command = /^(reglas|clk)$/i
+handler.help = ['4vs4 <hora> <modalidad>']
+handler.tags = ['ffvs']
+handler.command = /^(4x4|4vs4)$/i
 handler.admin = true
 handler.group = true
 
