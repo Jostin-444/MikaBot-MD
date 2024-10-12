087aec5a4b040b9e539ace32121b6699eeed66ec
Giovanni-OFC <jostingaleano10@gmail.com>
2024-10-12 13:27:13 -0600

    Actualizar reglas-CLK.js

--- plugins/reglas-CLK.js
+++ plugins/reglas-CLK.js
@@ -6,15 +6,15 @@ var handler = async (m, { conn, participants, groupMetadata, args, text }) => {
     const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net';
 
     // Verificar si se ingresaron suficientes argumentos (hora y modalidad)
-    if (args.length < 2) return m.reply('➤ `𝗔𝗩𝗜𝗦𝗢` ⚜️\n\n*INGRESA UNA HORA Y UNA MODALIDAD*\n_Ejemplo: .4vs4 10:00 Infinito_');
+    if (args.length < 2) return m.reply('➤ `𝗔𝗩𝗜𝗦𝗢` ⚜️\n\n*INGRESA UNA HORA Y UNA MODALIDAD*\n_Ejemplo: .4vs4_');
 
     const hora = args[0]; // Primer texto (hora)
     const modalidad = args.slice(1).join(' '); // Segundo texto (modalidad), puede contener más de una palabra
 
     m.react('🎮');
 
-    let yo = `│🕓 𝗛𝗢𝗥𝗔: *${hora}*`;
-    let modo = `│🎮 𝗠𝗢𝗗𝗔𝗟𝗜𝗗𝗔𝗗: *${modalidad}*`;
+    let yo = ``;
+    let modo = ``;
 
     let texto = `┍𝑹𝒆𝒈𝒍𝒂𝒔 𝑪𝑳𝑲🗒️
 ┕𝑯𝑨𝑩𝑰𝑳𝑰𝑫𝑨𝑫 𝑨𝑪𝑻𝑰𝑽𝑨: 𝑨𝑳𝑶𝑲
