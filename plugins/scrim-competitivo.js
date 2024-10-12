2b7c81fd40e45e16830130e640551625a906911f
Giovanni-OFC <jostingaleano10@gmail.com>
2024-10-12 14:43:08 -0600

    Actualizar plugins

---
+++ plugins/scrim-competitivo.js
@@ -0,0 +1,46 @@
+var handler = async (m, { conn, participants, groupMetadata, args, text }) => {
+
+    const pp = 'https://i.ibb.co/gFXNrPX/file.jpg';
+    const groupAdmins = participants.filter(p => p.admin);
+    const listaAdmins = groupAdmins.map((v, i) => ``).join('\n');
+    const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net';
+
+
+
+
+
+    m.react('🎮');
+
+    let texto = ` ╭──────⚔──────╮
+ㅤ𝐒𝐂𝐑𝐈𝐌𝐒 𝐂𝐎𝐌𝐏𝐄𝐓𝐈𝐓𝐈𝐕𝐎
+╰──────⚔──────╯
+╭──────────────╮
+│ㅤ⏱ 𝐇𝐎𝐑𝐀𝐑𝐈𝐎 
+│ㅤ🇲🇽 𝐌𝐄𝐗 : 
+│ㅤ🇨🇴 𝐂𝐎𝐋 : 
+│➥ 𝐓𝐎𝐓𝐀𝐋 𝐒𝐀𝐋𝐀𝐒:
+│➥ 𝐂𝐀𝐒𝐈𝐋𝐋𝐀:
+│➥ 𝐉𝐔𝐆𝐀𝐃𝐎𝐑𝐄𝐒:
+│
+│     𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 
+│
+│👑 ➤ 
+│🥷🏻 ➤  
+│🥷🏻 ➤ 
+│🥷🏻 ➤ 
+│
+│ㅤʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄:
+│🥷🏻 ➤  
+│🥷🏻 ➤
+╰─────────────╯`.trim();
+
+    conn.sendFile(m.chat, pp, 'error.jpg', texto, m, true, { mentions: [...groupAdmins.map(v => v.id), owner] });
+
+}
+handler.help = ['clk']
+handler.tags = ['ffvs']
+handler.command = /^(clk1|reglasclk)$/i
+handler.admin = true
+handler.group = true
+
+export default handler
