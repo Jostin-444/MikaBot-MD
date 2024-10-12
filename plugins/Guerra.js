4e359be6f86841b4a65b86cf065dfd5648856543
Giovanni-OFC <jostingaleano10@gmail.com>
2024-10-12 14:01:29 -0600

    Actualizar Guerra.js

---
+++ plugins/Guerra.js
@@ -0,0 +1,47 @@
+var handler = async (m, { conn, participants, groupMetadata, args, text }) => {
+
+    const pp = 'https://i.ibb.co/gFXNrPX/file.jpg';
+    const groupAdmins = participants.filter(p => p.admin);
+    const listaAdmins = groupAdmins.map((v, i) => ``).join('\n');
+    const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net';
+
+
+
+
+
+    m.react('🎮');
+
+    let texto = `
+╭┈┈ ๑❀๑ •𝐄𝐋𝐌𝐎-𝐁𝐎𝐓• ๑❀๑ ••:
+│
+│      ➤𝑬𝒏𝒄𝒂𝒓𝒈𝒂𝒅𝒙: 
+│      ➤ 𝑯𝒐𝒓𝒂𝒓𝒊𝒐: (  )🇵🇪 (  )🇦🇷
+│      ➤𝑴𝒐𝒅: 
+│
+│ㅤ➤𝑱𝒖𝒈𝒂𝒅𝒐𝒓𝒙𝒔:
+│
+│ㅤ💛 ▸
+│ㅤ💛 ▸
+│ㅤ💛 ▸
+│ㅤ💛 ▸
+│
+│ㅤ➤𝑺𝒖𝒑𝒍𝒆𝒏𝒕𝒆𝒔:
+│
+│ㅤ💛 ▸
+│ㅤ💛 ▸
+│
+│   ➤ 𝑫𝒐𝒏𝒂𝒅𝒐𝒓/𝒂 𝒅𝒆 𝒔𝒂𝒍𝒂:
+│
+╰───────────────๑❀๑ 
+`.trim();
+
+    conn.sendFile(m.chat, pp, 'error.jpg', texto, m, true, { mentions: [...groupAdmins.map(v => v.id), owner] });
+
+}
+handler.help = ['guerra']
+handler.tags = ['ffvs']
+handler.command = /^(guerra1|guerra)$/i
+handler.admin = true
+handler.group = true
+
+export default handler
